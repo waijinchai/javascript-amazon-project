98839330a87884681ab29babcdf5287ac5431a94
waijinchai <wcha0106@student.monash.edu>
2024-07-04 19:50:08 +0800

    Delete products from HTML when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -18,7 +18,7 @@ cart.forEach((cartItem) => {
     });
 
     cartSummaryHTML += `
-        <div class="cart-item-container">
+        <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
                 Delivery date: Tuesday, June 21
             </div>
@@ -104,6 +104,9 @@ document.querySelectorAll(".js-delete-link")
     .forEach((link) => {
         link.addEventListener("click", () => {
             const { productId } = link.dataset;
-            removeFromCart(productId)
+            removeFromCart(productId);
+
+            const container = document.querySelector(`.js-cart-item-container-${productId}`);
+            container.remove();
         })
     });
