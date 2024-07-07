4775464720bc848e27e616554f8ac65969f24e1a
waijinchai <wcha0106@student.monash.edu>
2024-07-07 23:55:30 +0800

    Load products from backend

--- data/products.js
+++ data/products.js
@@ -75,6 +75,31 @@ class Appliance extends Product {
   }
 }
 
+export let products = [];
+
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener("load", () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === "clothing") {
+        return new Clothing(productDetails);
+      } else if (productDetails.type === "appliance") {
+        return new Appliance(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    console.log("load products");
+
+    fun();
+  });
+
+  xhr.open("GET", "https://supersimplebackend.dev/products");
+  xhr.send();
+}
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -746,3 +771,4 @@ export const products = [
 
   return new Product(productDetails);
 });
+*/
